10386a928876d166edddbd4444a2ae0bc8050572
Nav Saini <nav@widgetic.com>
2021-11-06 16:53:04 +0530

    auto convert cursor to underscore on vim exit

--- private_dot_config/nvim/lua/core/autocmds.lua
+++ private_dot_config/nvim/lua/core/autocmds.lua
@@ -15,3 +15,6 @@ vim.cmd [[ autocmd BufEnter,BufWinEnter,FileType,WinEnter * lua require("core.ut
 -- vim.cmd [[ au BufReadPost * if expand('%:p') !~# '\m/\.git/' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif ]]
 -- File extension specific tabbing
 -- vim.cmd [[ autocmd Filetype python setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4 ]]
+--
+vim.cmd [[ au VimLeave * set guicursor=a:hor60-blinkon0 ]]
+
